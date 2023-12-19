3f61e8d553dcd60a61bda135bf547bfeb0cd5ba5
MachunuruSreenivasulaRedd <918296629y@gmail.com>
2023-12-19 13:08:18 +0530

    added payment

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
 import React from 'react'
-import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { BrowserRouter, Router, Route } from "react-router-dom"
 import LoginForm from './components/LoginForm'
 import Profile from './components/Profile'
 import Dashboard from './components/Dashboard'
 import Notifications from './components/Notifications'
 import Earnings from './components/Earnings'
+import Home from './components/Home'
 
 
 const App = () => 
    (
     <BrowserRouter>
-      <Routes>
-         <Route exact path="/" element={<LoginForm />}/>
+      <Router>
+         <Route exact path="/" element={<Home />}/>
+         <Route exact path="/login" element={<LoginForm />}/>
          <Route exact path="/profile" element={<Profile />}/>
          <Route exact path="/myearnings" element={<Earnings />} /> 
          <Route path="/dashboard" element={<Dashboard />} />
          <Route path="/notifications" element={<Notifications />} />
-      </Routes>
+      </Router>
       </BrowserRouter>
   )
 
